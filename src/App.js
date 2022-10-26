953b0633a23915b036fcbf08a78bf6edacfa91c6
sntechbio <lnascimentodesouza@gmail.com>
2022-10-26 15:32:10 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    numero1: null,
+    numero2: null,
+    resultado: null
+  }
+
+  render() {
+
+    return(
+      <div>
+        <label>Primeiro Número</label>
+        <input type="text" value={this.state.numero1} onChange={(e) => this.setState({numero1: e.target.value}) } />
+        <br />
+
+        <label>Segundo número: </label>
+        <input type="text" value={this.state.numero2} onChange={(e) => this.setState({numero2: e.target.value}) } />
+        <br/>
+
+        <button onClick={() => this.setState({resultado: parseInt(this.state.numero1) + parseInt(this.state.numero2)})}>Somar</button>
+        <br/>
+        O resultado é: {this.state.resultado}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
