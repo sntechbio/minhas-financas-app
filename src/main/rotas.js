77eaa11f4df7481d6bc95004868726ba5052d279
sntechbio <lnascimentodesouza@gmail.com>
2023-01-24 14:27:41 -0300

    componente home implementada

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Login from '../views/login'
+import Home from "../views/home";
 import { Route, Switch, HashRouter } from 'react-router-dom'
 import CadastroUsuario from "../views/cadastroUsuario";
 
@@ -7,6 +8,7 @@ function Rotas() {
     return (
         <HashRouter>
             <Switch>
+                <Route path='/home' component={Home} />
                 <Route path='/login' component={Login} />
                 <Route path='/cadastro-usuarios' component={CadastroUsuario} />
             </Switch>
