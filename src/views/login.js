6d4a7bf5a7c5bc1a0fd3e6fba04f065d6bfbd4ce
sntechbio <lnascimentodesouza@gmail.com>
2023-02-16 21:09:20 -0300

    implemtacao da autenticacao ok

--- src/views/login.js
+++ src/views/login.js
@@ -3,6 +3,7 @@ import Card from "../components/card";
 import FormGroup from "../components/form-group";
 import 'bootswatch/dist/flatly/bootstrap.css'
 import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 
 class Login extends React.Component {
 
@@ -12,8 +13,14 @@ class Login extends React.Component {
     }
 
     entrar = () => {
-        console.log('Email: ', this.state.email)
-        console.log('Senha:', this.state.senha)
+        axios.post('http://localhost:8080/api/usuarios/autenticar', {
+            email: this.state.email,
+            senha: this.state.senha
+        }).then( response => {
+            console.log( response )
+        }).catch( erro => {
+            console.log( erro.response )
+        })
     }
 
     prepareCadastrar = () => {
