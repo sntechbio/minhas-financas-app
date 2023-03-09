9268f065b1fa5608612f755f6a54433064aa5eca
sntechbio <lnascimentodesouza@gmail.com>
2023-03-09 15:27:34 -0300

    busca de lancamentos finalizadas

---
+++ src/app/service/lancamentoService.js
@@ -0,0 +1,31 @@
+import ApiService from "../apiservice"
+
+export default class LancamentoService extends ApiService {
+
+    constructor() {
+        super("/api/lancamentos")
+    }
+
+    consultar(LancamentoFiltro) {
+        let params = `?ano=${LancamentoFiltro.ano}`
+
+        if(LancamentoFiltro.mes){
+            params = `${params}&mes=${LancamentoFiltro.mes}`
+        }
+
+        if(LancamentoFiltro.tipo){
+            params = `${params}&tipo=${LancamentoFiltro.tipo}`
+        }
+
+        if(LancamentoFiltro.status){
+            params = `${params}&status=${LancamentoFiltro.status}`
+        }
+
+        if(LancamentoFiltro.usuario){
+            params = `${params}&usuario=${LancamentoFiltro.usuario}`
+        } 
+
+        return this.get(params)
+    }
+
+}
--- src/views/Lancamentos/consulta-lancamentos.js
+++ src/views/Lancamentos/consulta-lancamentos.js
@@ -4,8 +4,38 @@ import Card from "../../components/card";
 import FormGroup from "../../components/form-group";
 import SelectMenu from "../../components/selectMenu";
 import LancamentosTable from "./lancamentos-table";
+import LancamentoService from "../../app/service/lancamentoService";
 
 class ConsultaLancamentos extends React.Component{
+
+    state = {
+        ano: '',
+        mes: '',
+        tipo: '',
+        lancamentos: []
+    }
+
+    constructor(){
+        super();
+        this.service = new LancamentoService();
+    }
+
+    buscar = () => {
+        const usuarioLogado = JSON.parse(localStorage.getItem("usuario_logado"))
+        const lancamentoFiltro = {
+            ano: this.state.ano,
+            mes: this.state.mes,
+            tipo: this.state.tipo,
+            usuario: usuarioLogado.id
+        }
+
+        this.service.consultar(lancamentoFiltro)
+            .then(response => {
+                this.setState({ lancamentos: response.data })
+            }).catch(error => {
+                console.log(error)
+            })
+    }
     
     render(){
 
@@ -28,8 +58,8 @@ class ConsultaLancamentos extends React.Component{
 
         const tipo = [
             {label: 'Selecione...', value: ''},
-            {label: 'Despesa', value: 1},
-            {label: 'Receita', value: 2}
+            {label: 'Despesa', value: 'DESPESA'},
+            {label: 'Receita', value: 'RECEITA'}
         ]
 
         const lancamentos = [
@@ -42,21 +72,35 @@ class ConsultaLancamentos extends React.Component{
                 <div className="row">
                     <div className="col-md-6">
                         <div className="bs-component">
+
                             <FormGroup htmlFor="inputAno" label="Ano: *">
                                 <input 
                                     type="text" 
                                     className="form-control"
                                     id="inputAno"
+                                    value={this.state.ano}
+                                    onChange={e => this.setState({ano: e.target.value})}
                                     aria-describedby="emailHelp"
                                     placeholder="Digite o Ano"/>
                             </FormGroup>
+
                             <FormGroup htmlFor="inputMes" label="Mês: ">
-                                <SelectMenu id="inputMes" className="form-select" lista={meses}/>
+                                <SelectMenu id="inputMes"
+                                            value={this.state.mes}
+                                            onChange={e => this.setState({mes: e.target.value})}
+                                            className="form-select"
+                                            lista={meses}/>
                             </FormGroup>
+
                             <FormGroup htmlFor="inputTipo" label="Tipo: ">
-                                <SelectMenu id="inputTipo" className="form-select" lista={tipo}/>
+                                <SelectMenu id="inputTipo" 
+                                            value={this.state.tipo}
+                                            onChange={e => this.setState({tipo: e.target.value})}
+                                            className="form-select" 
+                                            lista={tipo}/>
                             </FormGroup>
-                            <button type="button" className="btn btn-success">Buscar</button>
+
+                            <button onClick={this.buscar} type="button" className="btn btn-success">Buscar</button>
                             <button type="button" className="btn btn-danger">Cadastrar</button>
                         </div>
                     </div>
@@ -64,7 +108,7 @@ class ConsultaLancamentos extends React.Component{
                 <br/>
                 <div className="row">
                     <div className="col-md-12">
-                        <LancamentosTable lancamentos={lancamentos}/>
+                        <LancamentosTable lancamentos={this.state.lancamentos}/>
                     </div>
                 </div>
 
